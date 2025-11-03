88b5786216e4721fd4d9a2dae39a4b35e8db1e7d
C-Reaper <gnu.c.reaper@gmail.com>
2025-11-03 13:36:01 +0100

    init commit

---
+++ code/Main.ll
@@ -0,0 +1,14 @@
+from io import *
+from sys import *
+from math import *
+
+function pow(a,b)
+    return math::pow(a,b)
+end
+
+function main(x)
+    --out = x * x
+    --return 1.0 / (1.0 + math::pow(2.71828,-x))
+    --return x * x
+    return pow(2.0,x)
+end
---
+++ src/Main.c
@@ -0,0 +1,73 @@
+#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/LuaLike.h"
+#include "/home/codeleaded/System/Static/Library/TransformedView.h"
+
+
+float Function_1D(float x){
+	return sinf(x);
+}
+
+
+TransformedView tv;
+LuaLike ll;
+
+void Setup(AlxWindow* w){
+	ll = LuaLike_New("./code/Main.ll","./bin");
+	tv = TransformedView_New((Vec2){ GetWidth(),GetHeight() });
+}
+
+void Update(AlxWindow* w){
+    tv.ZoomSpeed = 1.0f * w->ElapsedTime;
+	TransformedView_HandlePanZoom(&tv,window.Strokes,GetMouse());
+
+	Clear(BLACK);
+
+	Vec2 wvls = { 0.0f,TransformedView_ScreenWorldY(&tv,0.0f) };
+	Vec2 wvle = { 0.0f,TransformedView_ScreenWorldY(&tv,GetHeight()) };
+	Vec2 whls = { TransformedView_ScreenWorldX(&tv,0.0f),0.0f };
+	Vec2 whle = { TransformedView_ScreenWorldX(&tv,GetWidth()),0.0f };
+
+	Vec2 svls = TransformedView_WorldScreenPos(&tv,wvls);
+	Vec2 svle = TransformedView_WorldScreenPos(&tv,wvle);
+	Vec2 shls = TransformedView_WorldScreenPos(&tv,whls);
+	Vec2 shle = TransformedView_WorldScreenPos(&tv,whle);
+
+	RenderLine(svls,svle,WHITE,1.0f);
+	RenderLine(shls,shle,WHITE,1.0f);
+
+	Vec2 PosBefore = {};
+	for(int i = 0;i<GetWidth();i++){
+		Vec2 Pos = { i,0.0f };
+		
+		float x = TransformedView_ScreenWorldX(&tv,i);
+		
+        Variable ret = LuaLike_Function(&ll,"main",(Variable[]){ 
+            Variable_Make("x","float",(Double[]){ x },sizeof(Double),-1,NULL,NULL),
+            VARIABLE_END
+        });
+        // Variable_Print(&ret);
+        // printf("\n");
+        float y = *(Double*)Variable_Data(&ret);
+        Variable_Free(&ret);
+        //float y = x * x;//Function_1D(x);
+
+		Pos.y = TransformedView_WorldScreenY(&tv,-y);
+		
+		RenderLine(PosBefore,Pos,RED,1.0f);
+		PosBefore = Pos;
+	}
+
+	Vec2 p = TransformedView_ScreenWorldPos(&tv,GetMouse());
+	String str = String_Format("P: X: %f, Y: %f",p.x,p.y);
+	RenderCStrSize(str.Memory,str.size,0.0f,0.0f,WHITE);
+	String_Free(&str);
+}
+void Delete(AlxWindow* w){
+    LuaLike_Free(&ll);
+}
+
+int main(){
+    if(Create("LL - Geogebra",1920,1080,1,1,Setup,Update,Delete))
+        Start();
+    return 0;
+}
